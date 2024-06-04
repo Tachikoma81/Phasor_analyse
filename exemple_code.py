7fc29351cfbd5cd12a775f6a93ee3d83ffe912ad
Tachikoma81 <117389256+Tachikoma81@users.noreply.github.com>
2024-06-04 08:31:01 +0200

    Update exemple_code.py

--- exemple_code.py
+++ exemple_code.py
@@ -1,6 +1,6 @@
 from phasor2 import phasor as ph
 x=ph('C:/blala/blabla/yourimage',lamdbasmin=410)
-x.phasors=x.calcul_phasor()
+x.calcul_phasor()
 img=x.hsv()
 x.plot_phasor()
-x.display_img(img=)
+x.display_img(img=x)
